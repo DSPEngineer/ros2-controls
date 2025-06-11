9d89d1742ada1d830fc5520c0a8b4f4bfbc2cebb
Jeffrey Lamb <jeffrey.lamb@terex.com>
2025-06-11 14:35:48 -0700

    Add messages indicating we're in each of the hardware functions.

--- src/turtlebot3_hardware/src/turtlebot3_hardware.cpp
+++ src/turtlebot3_hardware/src/turtlebot3_hardware.cpp
@@ -1,6 +1,7 @@
 #include "turtlebot3_hardware/turtlebot3_hardware.hpp"
 
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 namespace turtlebot3_hardware
 {
@@ -14,6 +15,7 @@ rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn Turtle
   }
 
   // Initialize hardware, read parameters, etc.
+  RCLCPP_INFO(rclcpp::get_logger("TurtleBot3Hardware"), "on_init was called");
 
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
@@ -37,6 +39,7 @@ std::vector<hardware_interface::CommandInterface> TurtleBot3Hardware::export_com
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn TurtleBot3Hardware::on_activate(const rclcpp_lifecycle::State &)
 {
   // Start hardware communication
+  RCLCPP_INFO(rclcpp::get_logger("TurtleBot3Hardware"), "on_activate was called");
 
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
@@ -44,13 +47,15 @@ rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn Turtle
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn TurtleBot3Hardware::on_deactivate(const rclcpp_lifecycle::State &)
 {
   // Stop hardware communication
+  RCLCPP_INFO(rclcpp::get_logger("TurtleBot3Hardware"), "on_deactivate was called");
 
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
-hardware_interface::return_type TurtleBot3Hardware::read(const rclcpp::Time &, const rclcpp::Duration &)
+hardware_interface::return_type TurtleBot3Hardware::read(const rclcpp::Time &, const rclcpp::Duration & period)
 {
   // Read from hardware
+  RCLCPP_INFO(rclcpp::get_logger("TurtleBot3Hardware"), "read was called");
 
   return hardware_interface::return_type::OK;
 }
@@ -58,6 +63,7 @@ hardware_interface::return_type TurtleBot3Hardware::read(const rclcpp::Time &, c
 hardware_interface::return_type TurtleBot3Hardware::write(const rclcpp::Time &, const rclcpp::Duration &)
 {
   // Write to hardware
+  RCLCPP_INFO(rclcpp::get_logger("TurtleBot3Hardware"), "write was called");
 
   return hardware_interface::return_type::OK;
 }
