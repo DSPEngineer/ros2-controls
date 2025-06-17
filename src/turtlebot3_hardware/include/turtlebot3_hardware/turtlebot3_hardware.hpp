53a1fcc20f8c8806c4dda902ba0701042160c8f4
Peter Rose <peter.rose@terex.com>
2025-06-17 22:37:39 +0200

    set ports via URDF talk to HW, attempt to write velocity

--- src/turtlebot3_hardware/include/turtlebot3_hardware/turtlebot3_hardware.hpp
+++ src/turtlebot3_hardware/include/turtlebot3_hardware/turtlebot3_hardware.hpp
@@ -19,12 +19,16 @@
 
 namespace turtlebot3_hardware
 {
+
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
 class TurtlebotHardware : public hardware_interface::SystemInterface
 {
 public:
     RCLCPP_SHARED_PTR_DEFINITIONS(TurtlebotHardware)
+
+    std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+    std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
     CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
 
@@ -53,22 +57,40 @@ private:
     dynamixel::PacketHandler *packet_handler_;
 
     // Reader for synchronous position reads from all motors
-    std::unique_ptr<dynamixel::GroupSyncRead> present_position_reader_;
+    dynamixel::GroupSyncRead *present_position_reader_;
 
     // Reader for synchronous velocity reads from all motors
-    std::unique_ptr<dynamixel::GroupSyncRead> present_velocity_reader_;
+    dynamixel::GroupSyncRead *present_velocity_reader_;
 
     // Writer for synchronous velocity writes to all motors
-    std::unique_ptr<dynamixel::GroupSyncWrite> goal_velocity_writer_;
+    dynamixel::GroupSyncWrite *goal_velocity_writer_;
 
+    std::string usb_port_;
+    uint32_t baud_rate_;
+    uint8_t left_wheel_id_;
+    uint8_t right_wheel_id_;
     uint8_t dxl_error_ = 0;   
     int dxl_comm_result_ = COMM_TX_FAIL;       
-    uint8_t param_goal_position_[4];
+    uint8_t dxl_goal_velocity_[4];
+
+
+    std::vector<double> joint_commands_vel_;
+    std::vector<double> joint_states_pos_;
+    std::vector<double> joint_states_vel_;
+    std::vector<uint8_t> joint_ids_;
+    std::map<uint8_t,uint8_t*> joint_control_map_;
 
     int configure_dynamixel(uint8_t id);
     int eval_dxl_result(void);
     int stop_dynamixel(uint8_t id);
     int start_dynamixel(uint8_t id);
+    int set_joint_velocities();
+
+    /* 
+    CallbackReturn set_joint_positions();
+    CallbackReturn set_joint_velocities();
+    CallbackReturn set_joint_params();
+    */
 };
 
 } // namespace turtlebot3_hardware
--- src/turtlebot3_hardware/src/turtlebot3_hardware.cpp
+++ src/turtlebot3_hardware/src/turtlebot3_hardware.cpp
@@ -6,6 +6,7 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
 
 // Control table address
 #define ADDR_PRO_TORQUE_ENABLE          64                 // Control table address is different in Dynamixel model
@@ -19,7 +20,6 @@
 #define VELOCITY_CONTROL_MODE           1
 #define POSITION_CONTROL_MODE           3
 
-
 // Data Byte Length
 #define LEN_PRO_GOAL_POSITION           4
 #define LEN_PRO_PRESENT_POSITION        4
@@ -28,13 +28,6 @@
 
 // Protocol version
 #define PROTOCOL_VERSION                2.0                 // See which protocol version is used in the Dynamixel
-
-// Default setting
-#define DXL1_ID                         1                   // Dynamixel#1 ID: 1
-#define DXL2_ID                         2                   // Dynamixel#2 ID: 2
-#define BAUDRATE                        57600
-#define DEVICENAME                      "/dev/ttyUSB0"      // Check which port is being used on your controller
-                                                            // ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
 #define TORQUE_ENABLE                   1                   // Value for enabling the torque
 #define TORQUE_DISABLE                  0                   // Value for disabling the torque
@@ -66,31 +59,128 @@ namespace turtlebot3_hardware
             
             // On validation error - return CallbackReturn::ERROR
         }
+        // init hardware interface settings  
+        usb_port_ = info_.hardware_parameters["usb_port"];
+        baud_rate_ = std::stoi(info_.hardware_parameters["baud_rate"]);
+        left_wheel_id_ = std::stoi(info_.hardware_parameters["left_wheel_id"]);
+        right_wheel_id_ = std::stoi(info_.hardware_parameters["right_wheel_id"]);
+        
+        // init joint control buffersize
+        joint_commands_vel_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        joint_states_pos_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        joint_states_vel_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        joint_ids_.resize(info_.joints.size(), 0);
+        
+
+        // Init communication handlers
+        port_handler_ = dynamixel::PortHandler::getPortHandler(usb_port_.c_str());
+        packet_handler_ = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+        // Initialize GroupSyncWrite instance
+        goal_velocity_writer_ = new dynamixel::GroupSyncWrite(
+            port_handler_, packet_handler_, ADDR_PRO_GOAL_VELOCITY, LEN_PRO_GOAL_VELOCITY);
+
+        // Initialize Groupsyncread instance for Present variable reading
+        present_position_reader_ = new dynamixel::GroupSyncRead(
+            port_handler_, packet_handler_, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION);
+        present_velocity_reader_ = new dynamixel::GroupSyncRead(
+            port_handler_, packet_handler_, ADDR_PRO_PRESET_VELOCITY, LEN_PRO_PRESENT_VELOCITY);
+
+        for (const hardware_interface::ComponentInfo & joint : info_.joints) {
+            // Turtlebot3Hardware has exactly two states and one command interface on each joint
+            if (joint.command_interfaces.size() != 1) {
+            RCLCPP_FATAL(
+                get_logger(),
+                "Joint '%s' has %zu command interfaces found. 1 expected.", joint.name.c_str(),
+                joint.command_interfaces.size());
+            return CallbackReturn::ERROR;
+            }
+
+            if (joint.command_interfaces[0].name != hardware_interface::HW_IF_VELOCITY) {
+            RCLCPP_FATAL(
+                get_logger(),
+                "Joint '%s' have %s command interfaces found. '%s' expected.", joint.name.c_str(),
+                joint.command_interfaces[0].name.c_str(), hardware_interface::HW_IF_VELOCITY);
+            return CallbackReturn::ERROR;
+            }
+
+            if (joint.state_interfaces.size() != 2) {
+            RCLCPP_FATAL(
+                get_logger(),
+                "Joint '%s' has %zu state interface. 2 expected.", joint.name.c_str(),
+                joint.state_interfaces.size());
+            return CallbackReturn::ERROR;
+            }
+
+            if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION) {
+            RCLCPP_FATAL(
+                get_logger(),
+                "Joint '%s' have '%s' as first state interface. '%s' expected.",
+                joint.name.c_str(), joint.state_interfaces[0].name.c_str(),
+                hardware_interface::HW_IF_POSITION);
+            return CallbackReturn::ERROR;
+            }
+
+            if (joint.state_interfaces[1].name != hardware_interface::HW_IF_VELOCITY) {
+            RCLCPP_FATAL(
+                get_logger(),
+                "Joint '%s' have '%s' as second state interface. '%s' expected.",
+                joint.name.c_str(), joint.state_interfaces[1].name.c_str(),
+                hardware_interface::HW_IF_VELOCITY);
+            return CallbackReturn::ERROR;
+            }
+        }
+        
         return CallbackReturn::SUCCESS;
+    }
+
+    std::vector<hardware_interface::StateInterface> TurtlebotHardware::export_state_interfaces() {
+        RCLCPP_INFO_STREAM(get_logger(), "export_state_interfaces");
+        std::vector<hardware_interface::StateInterface> state_interfaces;
+        for (uint i = 0; i < info_.joints.size(); i++) {
+            state_interfaces.emplace_back(hardware_interface::StateInterface(
+                info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joint_states_pos_[i]));
+            state_interfaces.emplace_back(hardware_interface::StateInterface(
+                info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &joint_states_vel_[i]));
+        }
+        return state_interfaces;
+    }
+    std::vector<hardware_interface::CommandInterface> TurtlebotHardware::export_command_interfaces() {
+        RCLCPP_INFO_STREAM(get_logger(), "export_command_interfaces");
+        std::vector<hardware_interface::CommandInterface> command_interfaces;
+        for (uint i = 0; i < info_.joints.size(); i++) {
+            command_interfaces.emplace_back(hardware_interface::CommandInterface(
+                info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &joint_commands_vel_[i])); 
+        }
+        return command_interfaces;
     }
 
     CallbackReturn TurtlebotHardware::on_configure(const rclcpp_lifecycle::State & previous_state) {
         (void) previous_state;
         RCLCPP_INFO_STREAM(get_logger(), "on_configure():");
         
-        // Connect to Dynamixel
-        port_handler_ = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+        // Add motor IDs to the SyncRead objects
+        if (present_position_reader_->addParam(left_wheel_id_) != true) {
+            RCLCPP_FATAL(get_logger(), "Could not add param for left wheel position");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
+        if (present_position_reader_->addParam(right_wheel_id_) != true) {
+            RCLCPP_FATAL(get_logger(), "Could not add param for right wheel position");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
+        if (present_velocity_reader_->addParam(left_wheel_id_) != true) {
+            RCLCPP_FATAL(get_logger(), "Could not add param for left wheel velocity");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
+        if (present_velocity_reader_->addParam(right_wheel_id_) != true) {
+            RCLCPP_FATAL(get_logger(), "Could not add param for right wheel velocity");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
 
-        // Initialize PacketHandler instance
-        packet_handler_ = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
-
-        // Initialize GroupSyncWrite instance
-        dynamixel::GroupSyncWrite groupSyncWrite(
-            port_handler_, packet_handler_, ADDR_PRO_GOAL_POSITION, LEN_PRO_GOAL_POSITION);
-
-        // Initialize Groupsyncread instance for Present Position
-        dynamixel::GroupSyncRead groupSyncRead(
-            port_handler_, packet_handler_, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION);
-        
-        bool dxl_addparam_result = false;
+        joint_ids_[0] = left_wheel_id_;
+        joint_ids_[1] = right_wheel_id_; 
         
         RCLCPP_INFO_STREAM(get_logger(), "opening port");
-
         // Open port
         if (port_handler_->openPort()) {
             RCLCPP_INFO_STREAM(get_logger(), "port open success");
@@ -99,33 +189,23 @@ namespace turtlebot3_hardware
             return CallbackReturn::ERROR;
         }
         // Set port baudrate
-        if (port_handler_->setBaudRate(BAUDRATE)) {
+        if (port_handler_->setBaudRate(baud_rate_)) {
             RCLCPP_INFO_STREAM(get_logger(), "baudrate set success");
         } else {
             RCLCPP_INFO_STREAM(get_logger(), "baudrate setting failed");
             return CallbackReturn::ERROR;
         }
 
-        if (configure_dynamixel(DXL1_ID) != DXL_SUCCESS) {
-            return CallbackReturn::ERROR;
-        }
-        dxl_addparam_result = groupSyncRead.addParam(DXL1_ID);
-        if (dxl_addparam_result != true) {
-            RCLCPP_ERROR(get_logger(), "[ID:%03d] groupSyncRead addparam failed", DXL1_ID);
+        if (configure_dynamixel(left_wheel_id_) != DXL_SUCCESS) {
             return CallbackReturn::ERROR;
         } else {
-            RCLCPP_INFO(get_logger(), "Dynamixel#%d setup complet", DXL1_ID);
+            RCLCPP_INFO(get_logger(), "Dynamixel#%d setup complet", left_wheel_id_);
         }
 
-        if (configure_dynamixel(DXL2_ID) != DXL_SUCCESS) {
-            return CallbackReturn::ERROR;
-        }
-        dxl_addparam_result = groupSyncRead.addParam(DXL2_ID);
-        if (dxl_addparam_result != true) {
-            RCLCPP_ERROR(get_logger(), "[ID:%03d] groupSyncRead addparam failed", DXL2_ID);
+        if (configure_dynamixel(right_wheel_id_) != DXL_SUCCESS) {
             return CallbackReturn::ERROR;
         } else {
-            RCLCPP_INFO(get_logger(), "Dynamixel#%d setup complet", DXL2_ID);
+            RCLCPP_INFO(get_logger(), "Dynamixel#%d setup complet", right_wheel_id_);
         } 
         RCLCPP_INFO_STREAM(get_logger(), "Successfully configured");
         return CallbackReturn::SUCCESS;
@@ -134,17 +214,31 @@ namespace turtlebot3_hardware
     CallbackReturn TurtlebotHardware::on_activate(const rclcpp_lifecycle::State & previous_state) {
         (void) previous_state;
         RCLCPP_INFO_STREAM(get_logger(), "on_activate");
+        
         // activate motors
-        if (start_dynamixel(DXL1_ID) != DXL_SUCCESS) {
+        if (start_dynamixel(left_wheel_id_) != DXL_SUCCESS) {
             return CallbackReturn::ERROR;
         }
-        if (start_dynamixel(DXL2_ID) != DXL_SUCCESS) {
+        if (start_dynamixel(right_wheel_id_) != DXL_SUCCESS) {
             return CallbackReturn::ERROR;
         }
-
-        // Testing to spin wheels with a goal velocity
-        packet_handler_->write4ByteTxRx(port_handler_, DXL1_ID, ADDR_PRO_GOAL_VELOCITY, -100);
-        packet_handler_->write4ByteTxRx(port_handler_, DXL2_ID, ADDR_PRO_GOAL_VELOCITY, 100);
+        // reset goal velocity 
+        *(int32_t*)dxl_goal_velocity_ = 0;
+        // Add parameter storage for left wheel goal velocity
+        if (goal_velocity_writer_->addParam(left_wheel_id_, dxl_goal_velocity_) != true) {
+            RCLCPP_FATAL(get_logger(), "Could not add param for left wheel goal velocity");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
+        // Add parameter storage for right wheel goal velocity
+        if (goal_velocity_writer_->addParam(right_wheel_id_, dxl_goal_velocity_) != true) {
+            RCLCPP_FATAL(get_logger(), "Failed to add param for right wheel goal velocity");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
+        // deploy the entire package 
+        if (goal_velocity_writer_->txPacket() != COMM_SUCCESS) {
+            RCLCPP_FATAL(get_logger(), "Could not transmit SyncWrite data");
+            return hardware_interface::CallbackReturn::ERROR;
+        }
 
         return CallbackReturn::SUCCESS;
     }
@@ -152,19 +246,25 @@ namespace turtlebot3_hardware
     CallbackReturn TurtlebotHardware::on_deactivate(const rclcpp_lifecycle::State & previous_state) {
         (void) previous_state;
         RCLCPP_INFO_STREAM(get_logger(), "on_deactivate");
-        if (stop_dynamixel(DXL1_ID) != DXL_SUCCESS) {
+        if (stop_dynamixel(left_wheel_id_) != DXL_SUCCESS) {
             return CallbackReturn::ERROR;
         }
-        if (stop_dynamixel(DXL2_ID) != DXL_SUCCESS) {
+        if (stop_dynamixel(right_wheel_id_) != DXL_SUCCESS) {
             return CallbackReturn::ERROR;
         }
+        
+        // release port
+        port_handler_->closePort();
+
         return CallbackReturn::SUCCESS;
     }
 
     hardware_interface::return_type TurtlebotHardware::read(const rclcpp::Time & time, const rclcpp::Duration & period) {
-        (void) period;
         (void) time;
+        (void) period;
         RCLCPP_INFO_STREAM(get_logger(), "read()");
+
+
         return hardware_interface::return_type::OK;
     }
 
@@ -172,6 +272,14 @@ namespace turtlebot3_hardware
         (void) period;
         (void) time;
         RCLCPP_INFO_STREAM(get_logger(), "write");
+        /* */
+        for (uint i = 0; i < joint_ids_.size(); i++) {
+            *(int32_t*)dxl_goal_velocity_ = (int32_t)joint_commands_vel_[i] / 1000;
+            RCLCPP_INFO(get_logger(), "id:%d gloal_vel: %d ",joint_ids_[i], *(int32_t*)dxl_goal_velocity_);  
+            goal_velocity_writer_->changeParam(joint_ids_[i], dxl_goal_velocity_);
+        }
+        goal_velocity_writer_->txPacket();
+
         return hardware_interface::return_type::OK;
 
     }
@@ -232,6 +340,10 @@ namespace turtlebot3_hardware
         return DXL_SUCCESS;
     }
 
+    int TurtlebotHardware::set_joint_velocities() {
+        return DXL_SUCCESS;
+    }
+    
 } // namespace turtlebot3_hardware
 
 // Export as a plugin
